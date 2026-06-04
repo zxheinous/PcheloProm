f7f9e5adc1e1f40666ed0cf2891ac9d94d882734
123 <kireevak@mer.ci.nsu.ru>
2026-06-05 00:19:44 +0700

    add login

--- PcheloProm/App.xaml.cs
+++ PcheloProm/App.xaml.cs
@@ -1,14 +1,16 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
+﻿using System.Windows;
+using PcheloProm.Views;
 
 namespace PcheloProm
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
-    }
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
 
+            LoginWindow loginWindow = new LoginWindow();
+            loginWindow.Show();
+        }
+    }
 }
---
+++ PcheloProm/Views/LoginWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System.Linq;
+using System.Windows;
+using System.Windows.Input;
+using PcheloProm.Models;
+
+namespace PcheloProm.Views;
+
+public partial class LoginWindow : Window
+{
+    public LoginWindow()
+    {
+        InitializeComponent();
+    }
+    private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+        if (e.LeftButton == MouseButtonState.Pressed)
+            this.DragMove();
+    }
+
+    private void CloseButton_Click(object sender, RoutedEventArgs e)
+    {
+        Application.Current.Shutdown();
+    }
+
+    private void LoginButton_Click(object sender, RoutedEventArgs e)
+    {
+        string username = txtUsername.Text.Trim();
+        string password = txtPassword.Password;
+
+        if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+        {
+            lblError.Text = "Заполните все поля!";
+            return;
+        }
+
+        using (PcheloPromContext context = new PcheloPromContext())
+        {
+            var user = context.Users
+                .FirstOrDefault(u => u.Username == username && u.PasswordHash == password);
+
+            if (user != null)
+            {
+                if (!user.IsActive)
+                {
+                    lblError.Text = "Ваш аккаунт заблокирован администратором!";
+                    return;
+                }
+
+                MainWindow mainWindow = new MainWindow();
+                mainWindow.Show();
+
+                this.Close();
+            }
+            else
+            {
+                lblError.Text = "Неверный логин или пароль!";
+            }
+        }
+    }
+}
